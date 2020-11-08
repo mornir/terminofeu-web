6f35d63cac3103d7b423749cae50dc782fe634c8
mornir <jeromepott@hotmail.com>
2020-11-08 11:33:51 +0100

    Prototype of code to reduce API calls

--- components/Common/LangLink.vue
+++ components/Common/LangLink.vue
@@ -16,5 +16,17 @@ export default {
       required: true,
     },
   },
+  methods: {
+    switchLang() {
+      // Prevent API calls within term & entry pages
+      if (this.$route.name.match(/term|entry/g)) {
+        // https://github.com/nuxt-community/i18n-module/issues/68#issuecomment-475792915
+        this.$i18n.locale = this.code
+        window.history.replaceState('', '', this.switchLocalePath(this.code))
+      } else {
+        this.$router.push(this.switchLocalePath(this.code))
+      }
+    },
+  },
 }
 </script>
