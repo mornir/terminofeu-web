24a40034df8160365cd9ecef9c7b6edd37152e90
mornir <jeromepott@hotmail.com>
2021-04-24 20:09:44 +0200

    Update all deps

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-// import createClient from '@sanity/client'
-
 import de from './locales/de.json'
 import fr from './locales/fr.json'
 
@@ -9,6 +7,7 @@ const sanityConfig = {
   withCredentials: true,
   minimal: true,
   useCdn: true,
+  apiVersion: '2021-04-24',
 }
 
 // const sanityClient = createClient(sanityConfig)
@@ -18,7 +17,7 @@ export default {
   ssr: false,
 
   // Target (https://go.nuxtjs.dev/config-target)
-  //target: 'static',
+  // target: 'static',
 
   // https://nuxtjs.org/api/configuration-modern/
   modern: process.env.NODE_ENV === 'production',
@@ -56,7 +55,7 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
-    '@nuxtjs/sanity',
+    '@nuxtjs/sanity/module',
     '@nuxtjs/google-fonts',
     'svg-to-vue-component/nuxt',
   ],
