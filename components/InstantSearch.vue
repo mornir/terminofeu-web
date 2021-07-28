070a66292470de1c1c3f0a59f12f5729d85fe07e
mornir <jeromepott@hotmail.com>
2021-07-28 21:20:19 +0200

    Remove autofocus

--- components/InstantSearch.vue
+++ components/InstantSearch.vue
@@ -18,7 +18,6 @@
     <input
       id="search-box"
       v-debounce:300ms="search"
-      autofocus
       type="search"
       class="w-full px-8 py-4 text-xl font-semibold placeholder-gray-600 bg-orange-200 focus:text-white focus:outline-none focus:bg-primary-alt focus:placeholder-primary-alt"
       :placeholder="$t('searchTerm')"
