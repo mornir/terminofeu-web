64cb45e90a28c7f8da82a9b6074ef4aa427a16e1
mornir <jeromepott@hotmail.com>
2021-07-28 20:21:47 +0200

    style: smaller text on mobile

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <aside class="sticky top-0 z-10 py-1 bg-orange-400">
+  <aside class="sticky top-0 z-10 py-1 text-sm bg-orange-400 md:text-base">
     <p class="text-center"><slot /></p>
   </aside>
 </template>
