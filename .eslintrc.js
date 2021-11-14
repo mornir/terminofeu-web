4e36d44370b6438f411046e93c6ae2d00bcf0ed6
mornir <jeromepott@hotmail.com>
2021-11-14 13:53:09 +0100

    chore: upgrade all deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     'cypress/globals': true,
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false,
   },
   extends: [
     '@nuxtjs',
