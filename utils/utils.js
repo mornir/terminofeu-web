abba80ab70314ab08d78a9c286b8a1137c8da207
mornir <jeromepott@hotmail.com>
2021-11-14 20:31:03 +0100

    chore: remove unused function

--- utils/utils.js
+++ utils/utils.js
@@ -25,29 +25,4 @@ function generateTermsList(entries = []) {
   })
 }
 
-// TODO: Currently unused
-function prepareEntry(data) {
-  const entry = {
-    preferredTerm: {},
-    alternativeTerms: [],
-    definition: [],
-    relatedEntries: [],
-  }
-  entry.definition = data.definition
-
-  if (!data.terms?.length > 0) {
-    return entry
-  }
-
-  entry.preferredTerm = data.terms[0]
-  entry.alternativeTerms = data.terms.slice(1)
-
-  if (!data.relatedEntries?.length > 0) {
-    return entry
-  }
-
-  entry.relatedEntries = data.relatedEntries
-  return entry
-}
-
-export { prepareEntry, generateTermsList }
+export { generateTermsList }
